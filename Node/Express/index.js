fe73f40c2c489304174a97dacd51dfa19094934b
LuccasRibeiroNK <luckgl@hotmail.com>
2022-03-30 18:39:37 -0300

    Trabalhando com Express

---
+++ Node/Express/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.listen(4000, function (erro) {
+    if (erro) {
+        console.log("Erro ao iniciar o servidor");
+    } else {
+        console.log("Servidor iniciado com sucesso");
+    }
+});
