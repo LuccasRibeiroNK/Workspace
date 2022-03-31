82352bcd3140558924354e1ad1a570e9d1c5cb12
LuccasRibeiroNK <luckgl@hotmail.com>
2022-03-31 05:24:39 -0300

    .

--- Node/Express/index.js
+++ Node/Express/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Bem vindo!');
+});
+
+app.get('/blog', (req, res) => {
+    res.send('Bem vindo ao meu Blog');
+});
+
+app.get('/canal', (req, res) => {
+    res.send('Bem vindo ao meu Canal');
+})
+
+app.get("/canal/youtube", (req, res) => {
+    res.send("Bem vindo ao canal do Youtube");
+});
+
 app.listen(4000, function (erro) {
     if (erro) {
         console.log("Erro ao iniciar o servidor");
