724a71ea621bc19b16948db4364f3538f1d71f51
LuccasRibeiroNK <luckgl@hotmail.com>
2022-05-25 06:54:17 -0300

    updates

---
+++ React/login-firebase/src/components/App.js
@@ -0,0 +1,19 @@
+import Signup from "./Signup";
+import { Container } from "react-bootstrap";
+
+
+function App() {
+    return (
+        <>
+            <Container className="d-flex align-items-center justify-content-center"
+                style={{ height: "100vh" }}
+            >
+                <div className="w-100" style={{ maxWidth: "400px" }}>
+                    <Signup />
+                </div>
+            </Container>
+        </>
+    );
+}
+
+export default App;
